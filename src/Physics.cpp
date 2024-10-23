2c6194dd7d03df4ebc116824703b4fde0c77e755
Balar05 <arnaubalasch0506@gmail.com>
2024-10-23 23:58:04 +0200

    Debug mode in key F9

--- src/Physics.cpp
+++ src/Physics.cpp
@@ -219,7 +219,7 @@ bool Physics::PostUpdate()
 	bool ret = true;
 	
 	// Activate or deactivate debug mode
-	if (Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_F1) == KEY_DOWN) {
+	if (Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_F9) == KEY_DOWN) {
 		if (debug) {
 			debug = false;
 		}
