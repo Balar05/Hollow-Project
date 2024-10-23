e91d99af4aad9f1143ed84464991c48c25af5612
Balar05 <arnaubalasch0506@gmail.com>
2024-10-23 23:53:38 +0200

    Debug mode fixed
    The game starts with debug mode OFF.

--- vcpkg_installed/x64-windows/vcpkg/blds/pugixml/x64-windows-dbg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/arnaubl1/Documents/GitHub/Hollow-Project/vcpkg_installed/x64-windows/vcpkg/blds/pugixml/src/v1.14-3215fd4dab.clean
-# Build directory: C:/Users/arnaubl1/Documents/GitHub/Hollow-Project/vcpkg_installed/x64-windows/vcpkg/blds/pugixml/x64-windows-dbg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
